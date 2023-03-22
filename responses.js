9615fd415af661403a6b62f26a031faf6a0856dc
KrillNCrabs <128561100+KrillNCrabs@users.noreply.github.com>
2023-03-22 11:33:51 -0700

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,7 @@
+var responses = ["Yes", "No", "No U", "Probably", "Probably Not", "Never", "Always", "Ur Mom", "No Response", "Defintely", "On God", "Perhaps", "Maybe", "Defintely Not", "Roll Again", "Why?", "Try Again", "Most Likely Not", "Consult The Library Of Babel", "Reply Is Hazy, Try Again", "Concentrate And Ask Again", "It Is Certain", "Signs Point To Yes", "My Sources Say No", "By All Means", "Absolutely", "Of Course", "Negative", "Positive", "Yes, But Soon No", "No, But Soon Yes", "Don't Count On It", "Very Doubtful", "Not At All", "Your Answer Is Certain", "Your Answer Is Not Certain", "On God?"];
+var responsesf = ["Yes", "No", "Probably", "Probably Not", "Never", "Always", "No Response", "Defintely", "Perhaps", "Maybe", "Defintely Not", "Roll Again", "Why?", "Try Again", "Most Likely Not", "Consult The Library Of Babel", "Reply Is Hazy, Try Again", "Concentrate And Ask Again", "It Is Certain", "Signs Point To Yes", "My Sources Say No", "By All Means", "Absolutely", "Of Course", "Negative", "Positive", "Yes, But Soon No", "No, But Soon Yes", "Don't Count On It", "Very Doubtful", "Not At All", "Your Answer Is Certain", "Your Answer Is Not Certain"];
+
+function answer() {
+    let n = Math.floor(Math.random() * responsesf.length);
+    document.getElementById("responses").innerHTML = responsesf[n];
+}
