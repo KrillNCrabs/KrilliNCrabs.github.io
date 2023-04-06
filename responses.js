7db1a82b00fe089bd28da4211586f67cc11ebc29
KrillNCrabs <128561100+KrillNCrabs@users.noreply.github.com>
2023-04-06 11:11:29 -0700

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,36 +1,69 @@
-var responses = ["Yes", "No", "No U", "Probably", "Probably Not", "Never", "Always", "Ur Mom", "No Response", "Defintely", "On God", "Perhaps", "Maybe", "Defintely Not", "Roll Again", "Why?", "Try Again", "Most Likely Not", "Consult The Library Of Babel", "Reply Is Hazy, Try Again", "Concentrate And Ask Again", "It Is Certain", "Signs Point To Yes", "My Sources Say No", "By All Means", "Absolutely", "Of Course", "Negative", "Positive", "Yes, But Soon No", "No, But Soon Yes", "Don't Count On It", "Very Doubtful", "Not At All", "Your Answer Is Certain", "Your Answer Is Not Certain", "On God?"];
-var responsesf = ["Yes", "No", "Probably", "Probably Not", "Never", "Always", "No Response", "Defintely", "Perhaps", "Maybe", "Defintely Not", "Roll Again", "Why?", "Try Again", "Most Likely Not", "Consult The Library Of Babel", "Reply Is Hazy, Try Again", "Concentrate And Ask Again", "It Is Certain", "Signs Point To Yes", "My Sources Say No", "By All Means", "Absolutely", "Of Course", "Negative", "Positive", "Yes, But Soon No", "No, But Soon Yes", "Don't Count On It", "Very Doubtful", "Not At All", "Your Answer Is Certain", "Your Answer Is Not Certain", "Affirmative", "Of Course Not", "Absolutely Not", "Under No Circumstances", "Indeed", "Are You Sure?", "Without A Doubt", "It Is Decidedly So", "You May Rely On It", "As I See It, Yes", "Most Likely", "Better Not Tell You Now", "Cannot Predict Now", "Outlook Not So Good", "My Reply Is Yes", "My Reply Is No", "...", "The Spirits Say Yes", "The Spirits Say No", "The Prediction Is Foggy", "Try Thinking It Over", "Possibly In The Future", "Better If You Did Not Know"];
-const outer = document.getElementById("blackShell"); 
-const inner = document.getElementById("whiteShell"); 
+const responsesYes = ["Yes","Probably","Always","Defintely","Perhaps", "Maybe","It Is Certain", "Signs Point To Yes","By All Means", "Absolutely", "Of Course","Positive","No, But Soon Yes","Your Answer Is Certain","Affirmative","Indeed","Without A Doubt", "It Is Decidedly So", "You May Rely On It", "As I See It, Yes", "Most Likely","My Reply Is Yes","The Spirits Say Yes","Possibly In The Future", "Surely", "Naturally", "Obviously","Clearly"];
+const responsesNo = ["No","Probably Not","Never","Defintely Not","Most Likely Not","My Sources Say No","Negative","Yes, But Soon No","Don't Count On It", "Very Doubtful", "Not At All","Your Answer Is Not Certain","Of Course Not", "Absolutely Not", "Under No Circumstances","Outlook Not So Good","My Reply Is No","The Spirits Say No","Not Ever"];
+const responsesUnknown = ["No Response","Roll Again", "Why?", "Try Again","Consult The Library Of Babel", "Reply Is Hazy, Try Again", "Concentrate And Ask Again","Are You Sure?","Better Not Tell You Now", "Cannot Predict Now","...","Prediction Is Foggy", "Try Thinking It Over","Better If You Did Not Know", "Consult The Spirits", "Avoid That Question"];
+const outer = document.getElementById("blackShell");
+const inner = document.getElementById("whiteShell");
 const eight = document.getElementById("eight");
-function answer() {
-let n = Math.floor(Math.random() * responsesf.length); 
-document.getElementById("responses").innerHTML = responsesf[n];
+
+function answer(){
+    let n = Math.floor(Math.random()*7);
+    let z = 0;
+    switch(n){
+        case 0:
+            z = Math.floor(Math.random()*(responsesUnknown.length));
+            document.getElementById("responses").innerHTML = responsesUnknown[z];
+            break;
+            case 1:
+            z = Math.floor(Math.random()*(responsesYes.length));
+            document.getElementById("responses").innerHTML = responsesYes[z];
+            break;
+            case 2:
+            z = Math.floor(Math.random()*(responsesYes.length));
+            document.getElementById("responses").innerHTML = responsesYes[z];
+            break;
+            case 3:
+            z = Math.floor(Math.random()*(responsesYes.length));
+            document.getElementById("responses").innerHTML = responsesYes[z];
+            break;
+            case 4:
+            z = Math.floor(Math.random()*(responsesNo.length));
+            document.getElementById("responses").innerHTML = responsesNo[z];
+            break;
+            case 5:
+            z = Math.floor(Math.random()*(responsesNo.length));
+            document.getElementById("responses").innerHTML = responsesNo[z];
+            break;
+            case 6:
+            z = Math.floor(Math.random()*(responsesNo.length));
+            document.getElementById("responses").innerHTML = responsesNo[z];
+            break;
+    }
+    
 }
-var w = screen.width 
+var w = screen.width;
 if (w < 1000) {
-outer.style.width = "70vw"; 
-outer.style.height = "70vw"; 
-inner.style.width = "50vw"; 
-inner.style.height = "50vw"; 
-eight.style.fontSize = "45vw";
+    outer.style.width = "70vw";
+    outer.style.height = "70vw";
+    inner.style.width = "50vw";
+    inner.style.height = "50vw";
+    eight.style.fontSize = "45vw";
 }
 window.addEventListener("resize", detects);
+
 function detects() {
-let w = screen.width;
- let h = screen.height; 
-if (w > h) {
-outer.style.width = "37vw"; 
-outer.style.height = "37vw"; 
-inner.style.width = "26vw"; 
-inner.style.height = "26vw";
-eight.style.fontSize = "26vw"; 
-} else if (w < h) {
-outer.style.width = "70vw"; 
-outer.style.height = "70vw"; 
-inner.style.width = "50vw"; 
-inner.style.height = "50vw"; 
-eight.style.fontSize = "45vw";
-} 
+    let w = screen.width;
+    let h = screen.height;
+    if (w > h) {
+        outer.style.width = "37vw";
+        outer.style.height = "37vw";
+        inner.style.width = "26vw";
+        inner.style.height = "26vw";
+        eight.style.fontSize = "26vw";
+    } else if (w < h) {
+        outer.style.width = "70vw";
+        outer.style.height = "70vw";
+        inner.style.width = "50vw";
+        inner.style.height = "50vw";
+        eight.style.fontSize = "45vw";
+    }
 }
- 
